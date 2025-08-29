97d6a20c6c4ede233f01bc5521259f186b5e2390
llarchev <lucaslarchevque@gmail.com>
2025-08-29 15:12:18 +0200

    filter first asset

--- internal/service/filter.go
+++ internal/service/filter.go
@@ -1,1 +1,23 @@
 package service
+
+import (
+	"groupie-tracker/internal/models"
+	"strings"
+)
+
+// Filtrer les artistes dont le nom contient la chaîne recherchée (insensible à la casse)
+func filterArtists(artists []models.Artist, query string) []models.Artist {
+	if query == "" {
+		return artists // pas de recherche → renvoie tout
+	}
+
+	query = strings.ToLower(query)
+	var result []models.Artist
+
+	for _, artist := range artists {
+		if strings.Contains(strings.ToLower(artist.Name), query) {
+			result = append(result, artist)
+		}
+	}
+	return result
+}
